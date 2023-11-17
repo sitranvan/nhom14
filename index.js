485aa05dc3b3024fa7d1ce5a101ce03b5003c7b9
Si Tran Van <it.sitranvan@gmail.com>
2023-11-17 21:43:53 +0700

    khoi tao

---
+++ index.js
@@ -0,0 +1,24 @@
+// Hàm kiểm tra nguyên tố - Trần Văn Sĩ
+function ktSoNguyenTo() {
+    
+}
+// Võ Quốc Thắng - Hàm tổng
+function tong() {
+
+}
+// Trang Thanh Hải - Hàm hiệu
+function hieu() {
+    
+}
+// Nguyễn Hoàng Khang - Hàm tích
+function tich() {
+    
+}
+
+// Phạm Ngọc Đông - Hàm thương
+
+function thuong() {
+    
+}
+
+// Gọi hàm
